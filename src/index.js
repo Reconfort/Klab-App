45e3dcc8d22ac954cf30785a4d37da9aaff3f872
Reconfort Daniel <75314029+Reconfort@users.noreply.github.com>
2023-03-15 16:24:39 +0200

    Remove Device

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Device from './Components/Device';
+// import Device from './Components/Device';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Device/>
+//     <Device/>
   </React.StrictMode>
 );
